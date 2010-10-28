495fb5ceaf806393de5ebb9d5aa276b26b896295
gzhou <gzhou@msp.tfs.local>
2010-10-28 19:39:20 +0000

    Update for overlap class
    git-tfs-id: [https://devops.cyberoptics.com/TPC1]$/CyberStitch;C29292

--- Main/src/logic/ImageCorrelation/OverlapDefines.cpp
+++ Main/src/logic/ImageCorrelation/OverlapDefines.cpp
@@ -1,2 +1,234 @@
 #include "OverlapDefines.h"
 
+#pragma region CorrelationResult class
+CorrelationResult::CorrelationResult()
+{
+	RowOffset = 0;
+	ColOffset = 0;
+	CorrCoeff = 0;
+	AmbigScore = -1;
+}
+
+CorrelationResult::CorrelationResult(
+	double		dRowOffset,
+	double		dColOffset,
+	double		dCorrCoeff,
+	double		dAmbigScore)
+{
+	RowOffset	= dRowOffset;
+	ColOffset	= dColOffset;
+	CorrCoeff	= dCorrCoeff;
+	AmbigScore	= dAmbigScore;
+}
+
+CorrelationResult::CorrelationResult(const CorrelationResult& b)
+{
+	*this = b;
+}
+
+void CorrelationResult::operator=(const CorrelationResult& b)
+{
+	RowOffset	= b.RowOffset;
+	ColOffset	= b.ColOffset;
+	CorrCoeff	= b.CorrCoeff;
+	AmbigScore	= b.AmbigScore;
+}
+#pragma endregion
+
+#pragma region CorrelationPair class
+
+CorrelationPair::CorrelationPair()
+{
+	_pImg1 = NULL;
+	_pImg2 = NULL;
+	_pMaskImage = NULL;
+	
+	_type = NULL_OVERLAP;
+
+	_bIsProcessed=false;
+}
+
+CorrelationPair::CorrelationPair(
+		Image* pImg1, 
+		Image* pImg2, 
+		UIRect roi1, 
+		pair<unsigned int, unsigned int> topLeftCorner2,
+		OverlapType type,
+		Image* pMaskImg)
+{
+	_pImg1 = pImg1;
+	_pImg2 = pImg2;
+	_pMaskImage = pMaskImg;
+
+	_roi1 = roi1;	
+	_roi2.FirstColumn = topLeftCorner2.first;
+	_roi2.FirstRow = topLeftCorner2.second;
+	_roi2.LastColumn = _roi2.FirstColumn + _roi1.Columns() - 1;
+	_roi2.LastRow = _roi2.FirstRow + _roi1.Rows() -1;
+
+	_type = type;
+
+	_bIsProcessed = false;
+}
+
+CorrelationPair::CorrelationPair(const CorrelationPair& b)
+{
+	*this = b;
+}
+
+void CorrelationPair::operator=(const CorrelationPair& b)
+{
+	_pImg1 = b._pImg1;
+	_pImg2 = b._pImg2;
+	_pMaskImage = b._pMaskImage;
+
+	_roi1 = b._roi1;
+	_roi2 = b._roi2;
+
+	_type = b._type;
+
+	_bIsProcessed = b._bIsProcessed;
+	_result = b._result;
+}
+
+// Return true if result is available 
+bool CorrelationPair::GetCorrelationResult(CorrelationResult* pResult)
+{
+	if(!_bIsProcessed)
+		return(false);
+
+	*pResult = _result;
+
+	return(true);
+}
+
+#pragma endregion
+
+#pragma region Overlap class
+
+Overlap::Overlap()
+{
+	_bValid = false;
+}
+
+Overlap::Overlap(
+		Image* pImg1, 
+		Image* pImg2,
+		DRect validRect,
+		OverlapType type)
+{
+	_pImg1 = pImg1, 
+	_pImg2 = pImg2, 
+
+	_validRect = validRect;
+
+	_type = type;
+
+	_bValid = CalCoarseCorrPair();
+}
+
+Overlap::Overlap(const Overlap& overlap) 
+{
+	*this = overlap;
+}
+
+void Overlap::operator=(const Overlap& b)
+{
+	_pImg1 = b._pImg1, 
+	_pImg2 = b._pImg2, 
+
+	_validRect = b._validRect;
+
+	_type = b._type;
+
+	_bValid = b._bValid;
+	_coarsePair = b._coarsePair;
+}
+
+// Calculate the coarse correlation pair
+bool Overlap::CalCoarseCorrPair()
+{
+	// Get image's bound box in world space
+	DRect rectWorld1 = _pImg1->GetBoundBoxInWorld();
+	DRect rectWorld2 = _pImg2->GetBoundBoxInWorld();
+
+	// Calculate the overlap of image in world space
+	DRect overlapWorld;
+	overlapWorld.xMin = rectWorld1.xMin>rectWorld2.xMin ? rectWorld1.xMin : rectWorld2.xMin;
+	overlapWorld.xMax = rectWorld1.xMax<rectWorld2.xMax ? rectWorld1.xMax : rectWorld2.xMax;
+	
+	overlapWorld.yMin = rectWorld1.yMin>rectWorld2.yMin ? rectWorld1.yMin : rectWorld2.yMin;
+	overlapWorld.yMax = rectWorld1.yMax<rectWorld2.yMax ? rectWorld1.yMax : rectWorld2.yMax;
+
+	if(overlapWorld.xMin < _validRect.xMin) overlapWorld.xMin = _validRect.xMin;
+	if(overlapWorld.xMax > _validRect.xMax) overlapWorld.xMax = _validRect.xMax;
+	if(overlapWorld.yMin < _validRect.yMin) overlapWorld.yMin = _validRect.yMin;
+	if(overlapWorld.yMax > _validRect.yMax) overlapWorld.yMax = _validRect.yMax;
+
+		// validation check
+	if(overlapWorld.xMin>=overlapWorld.xMax ||
+		overlapWorld.yMin>=overlapWorld.yMax)
+		return(false);
+
+	// Calculate Roi1 and Roi2
+		// Rows and colums
+	unsigned int iRows, iCols;
+	if(_pImg1->PixelSizeX() > _pImg2->PixelSizeX())
+		iRows = (unsigned int)((overlapWorld.xMax - overlapWorld.xMin)/_pImg1->PixelSizeX());
+	else
+		iRows = (unsigned int)((overlapWorld.xMax - overlapWorld.xMin)/_pImg2->PixelSizeX());
+
+	if(_pImg1->PixelSizeY() > _pImg2->PixelSizeY())
+		iCols = (unsigned int)((overlapWorld.yMax - overlapWorld.yMin)/_pImg1->PixelSizeY());
+	else
+		iCols = (unsigned int)((overlapWorld.yMax - overlapWorld.yMin)/_pImg2->PixelSizeY());
+
+	UIRect roi1, roi2;
+	double dFirstRow1, dFirstCol1, dFirstRow2, dFirstCol2;
+	_pImg1->WorldToImage(overlapWorld.xMin, overlapWorld.yMin, &dFirstRow1, &dFirstCol1);
+	_pImg2->WorldToImage(overlapWorld.xMin, overlapWorld.yMin, &dFirstRow2, &dFirstCol2);
+		
+		// Roi1 and Roi2
+	roi1.FirstRow = (unsigned int)dFirstRow1;
+	roi1.LastRow = roi1.FirstRow+iRows-1;
+	roi1.FirstColumn = (unsigned int)dFirstCol1;
+	roi1.LastColumn = roi1.FirstColumn+iCols-1;
+
+	roi2.FirstRow = (unsigned int)dFirstRow2;
+	roi2.LastRow = roi2.FirstRow+iRows-1;
+	roi2.FirstColumn = (unsigned int)dFirstCol2;
+	roi2.LastColumn = roi2.FirstColumn+iCols-1;
+
+		// Validation check 
+	if(roi1.FirstRow < 0 ||
+		roi1.LastRow > _pImg1->Rows()-1 ||
+		roi1.FirstColumn < 0 ||
+		roi1.LastColumn > _pImg1->Columns()-1)
+	{
+		//G_LOG_0_ERROR("CameraOverlap failed BoundsCheck");
+	}
+
+	if(roi2.FirstRow < 0 ||
+		roi2.LastRow > _pImg2->Rows()-1 ||
+		roi2.FirstColumn < 0 ||
+		roi2.LastColumn > _pImg2->Columns()-1)
+	{
+		//G_LOG_0_ERROR("CameraOverlap failed BoundsCheck");
+	}
+
+	CorrelationPair coarsePair(_pImg1, _pImg2, roi1, pair<unsigned int, unsigned int>(roi2.FirstRow, roi2.FirstColumn), _type);
+	_coarsePair = coarsePair;
+
+	return(true);
+}
+
+
+#pragma endregion
+
+#pragma region FovFovOverlap
+
+
+#pragma endregion 
+
+
+
--- Main/src/logic/ImageCorrelation/OverlapDefines.h
+++ Main/src/logic/ImageCorrelation/OverlapDefines.h
@@ -3,19 +3,22 @@
 #include "MosaicImage.h"
 #include "UIRect.h"
 
+// Contains the correlation result
 class CorrelationResult
 {
 public:
 
 	CorrelationResult();
 
-	void operator=(const CorrelationResult& b)
-	{
-		RowOffset	= b.RowOffset;
-		ColOffset	= b.ColOffset;
-		CorrCoeff	= b.CorrCoeff;
-		AmbigScore	= b.AmbigScore;
-	}
+	CorrelationResult(
+		double		dRowOffset,
+		double		dColOffset,
+		double		dCorrCoeff,
+		double		dAmbigScore);
+
+	CorrelationResult(const CorrelationResult& b);
+
+	void operator=(const CorrelationResult& b);
 
 	// CorrelationResult properties
 	double		RowOffset;
@@ -24,62 +27,95 @@ public:
 	double		AmbigScore;
 };
 
-
 typedef enum { 
 	Row_To_Row			= 10,
 	Col_To_Col			= 20,
 	CAD					= 30,
 	FID					= 35,
 	NULL_OVERLAP		= 40
- } OverlapType;
+} OverlapType;
 
+// The image and ROI pair for correlation (contains result)
 class CorrelationPair
 {
 public:
+	CorrelationPair();
+
 	CorrelationPair(
 		Image* pImg1, 
 		Image* pImg2, 
 		UIRect roi1, 
-		UIRect roi2,
-		OverlapType type);
+		pair<unsigned int, unsigned int> topLeftCorner2,
+		OverlapType type,
+		Image* pMaskImage = NULL);
+
+	CorrelationPair(const CorrelationPair& b);
+
+	void operator=(const CorrelationPair& b);
 
 	Image* GetFirstImg()  {return _pImg1;};
 	Image* GetSecondIMg() {return _pImg2;};
 	UIRect GetFirstRoi()  {return _roi1;};
 	UIRect GetSecondRoi() {return _roi2;};
 
-	CorrelationResult GetCorrelationResult();
+	Image* GetMaskImg() {return _pMaskImage;};
+
+	bool GetCorrelationResult(CorrelationResult* pResult);
+
+	OverlapType GetOverlapType() {return _type;};
 
 private:
 	Image* _pImg1;
 	Image* _pImg2;
+	Image* _pMaskImage;
 
 	UIRect _roi1;
 	UIRect _roi2;
 
-	OverlapType _Type;
+	OverlapType _type;
 
-	bool		IsProcessed;
+	bool _bIsProcessed;
 
 	CorrelationResult _result;
 };
 
+// Base class for overlap between image and image
 class Overlap		
 {
 public:
+	Overlap();
+
 	Overlap(
 		Image* pImg1, 
-		Image* pImg2, 
-		UIRect roi1, 
-		UIRect roi2,
+		Image* pImg2,
+		DRect validRect,
 		OverlapType type);
 
+	Overlap(const Overlap& b);
+	void operator=(const Overlap& b);
+
+	bool IsValid() {return _bValid;};
+
+	bool DoIt();
+
+protected:
+	bool CalCoarseCorrPair();
+	bool ChopOverlap();
+
 private:
-	CorrelationPair coarsePair;
-	CorrelationPair* finePairs;
+	Image* _pImg1;
+	Image* _pImg2;
+	DRect _validRect;
+	OverlapType _type;
+
+	bool _bValid;
+
+	CorrelationPair _coarsePair;
+	list<CorrelationPair> _finePairList;
 };
 
-class ImgImgOverlap: public Overlap
+// Overlap between FOV image FOV image
+class FovFovOverlap: public Overlap
 {
 	MosaicImage*	pMosaic1;
 	MosaicImage*	pMosaic2;
@@ -87,7 +123,7 @@ class ImgImgOverlap: public Overlap
 	pair<unsigned int, unsigned int> ImgPos2;
 };
 
-class ImgCadOverlap: public Overlap
+class FovCadOverlap: public Overlap
 {
 	MosaicImage*	pMosaic;
 	pair<unsigned int, unsigned int> ImgPos;
@@ -96,7 +132,7 @@ class ImgCadOverlap: public Overlap
 };
 
 
-class ImgFidOverlap: public Overlap
+class FovFidOverlap: public Overlap
 {
 	MosaicImage*	pMosaic;
 	pair<unsigned int, unsigned int> ImgPos;
