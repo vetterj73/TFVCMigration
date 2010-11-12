6d134aa87663812d758e5fc4c48fb81933dfec72
Alan Torborg <atorborg@cyberoptics.com>
2010-11-12 22:29:13 +0000

    code commenting/refactoring logger.
    git-tfs-id: [https://devops.cyberoptics.com/TPC1]$/CyberStitch;C29926

--- Main/src/logic/Shared/LoggableManagedObject.h
+++ Main/src/logic/Shared/LoggableManagedObject.h
@@ -2,6 +2,10 @@
 #include "LoggableObject.h"
 using namespace System;
 using namespace System::Runtime::InteropServices;
+
+///
+///	This is used as a helper class to bubble up logging information to the managed layer.
+///
 namespace MLOGGER
 {
 	///
@@ -16,40 +20,78 @@ namespace MLOGGER
 		NUMLOGTYPES
 	};
 
+	///
+	///	Delegate - unmanaged to managed.
+	///
 	protected delegate void LoggingDelegateForUnmanaged(const char *message, LOGTYPE logType);
+	
+	///
+	///	Delegate to managed world.
+	///
 	public delegate void LoggingDelegate(String ^ message, MLOGTYPE logType);
 
+	///
+	///	Wrapper for an unmanaged LoggableObject
+	///
 	public ref class MLoggableObject
 	{
 		public:
+			///
+			///	Constructor - Hooks up the unmanaged delegate
+			///
 			MLoggableObject()
 			{
 				_pLoggableObject = NULL;
 				_loggingDelegate = gcnew LoggingDelegateForUnmanaged(this, &MLOGGER::MLoggableObject::RaiseLogEntry); 
 			}
 
+			///
+			/// Turns on/off a particular log type.
+			///
 			void SetLogType(MLOGTYPE logType, bool bOn)
 			{
 				if(_pLoggableObject)
 					_pLoggableObject->SetLogType((LOGTYPE)logType, bOn);
 			}
 
+			///
+			///	Turns on/off all log types.
+			///
+			void SetAllLogTypes(bool bOn)
+			{
+				if(_pLoggableObject)
+					_pLoggableObject->SetAllLogTypes(bOn);	
+			}
+
+			///
+			///	Managed Event Delegate
+			///
 			event LoggingDelegate^ OnLogEntry;
 
 		protected:
+
+			///
+			/// Unmanaged Loggable Object...
+			///
+			LoggableObject* _pLoggableObject;
+			
+			///
+			///	This allows the managed class to hook in to the unmanaged loggable object.
+			///
 			void SetLoggableObject(LoggableObject *pLoggableObject)
 			{
 				_pLoggableObject = pLoggableObject;
 				_pLoggableObject->RegisterLoggingCallback((LOGGINGCALLBACK)Marshal::GetFunctionPointerForDelegate(_loggingDelegate).ToPointer());
 			}
 
+			///
+			/// Plumbing from unmanaged to managed.
+			///
 			LoggingDelegateForUnmanaged ^_loggingDelegate;
 			void RaiseLogEntry(const char* error, LOGTYPE logType)
 			{
 				String ^msg = gcnew String(error);
 				OnLogEntry(msg, (MLOGTYPE)logType);
 			}
-
-			LoggableObject* _pLoggableObject;
 	};
 }
