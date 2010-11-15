d0702d1c48ba43ddf7fc35f86b0a9c5ebe674250
Alan Torborg <atorborg@cyberoptics.com>
2010-11-15 17:11:06 +0000

    Updates for offsets...
    git-tfs-id: [https://devops.cyberoptics.com/TPC1]$/CyberStitch;C29960

--- Main/src/Applications/CyberStitchTester/Program.cs
+++ Main/src/Applications/CyberStitchTester/Program.cs
@@ -98,32 +98,65 @@ namespace CyberStitchTester
         /// </summary>
         private static void SetupMosaic()
         {
-            ManagedSIMDevice d = ManagedCoreAPI.GetDevice(0);
-            if(d == null)
+            if (ManagedCoreAPI.NumberOfDevices() <= 0)
             {
                 Output("No Device Defined");
                 return;
             }
-
-            ManagedSIMCaptureSpec pSpec = d.GetSIMCaptureSpec(0);
-            if(pSpec == null)
-            {
-                Output("No Capture Specs defined");
-                return;
-            }
-
-            int numCameras = 0;
-            for (int i = 0; i < d.NumberOfCameras; i++ )
-                if (d.GetSIMCamera(i).Status() == (CameraStatus)1)
-                    numCameras++;
             _mosaicSet = new ManagedMosaicSet(.200, .250, 2592, 1944, 2592, .00017, .00017);
             _mosaicSet.OnImageAdded += OnImageAddedToMosaic;
             _mosaicSet.OnLogEntry += OnLogEntryFromMosaic;
             _mosaicSet.SetLogType(MLOGTYPE.LogTypeDiagnostic, true);
 
-            for (int i = 0; i < d.NumberOfCaptureSpecs; i++ )
+            for(int i=0; i<ManagedCoreAPI.NumberOfDevices(); i++)
+                AddDeviceToMosaic(ManagedCoreAPI.GetDevice(i));
+        }
+
+        private static void AddDeviceToMosaic(ManagedSIMDevice d)
+        {
+            ManagedSIMCaptureSpec pSpec = d.GetSIMCaptureSpec(0);
+            if (pSpec == null)
             {
-                _mosaicSet.AddLayer(.2, i*.2, numCameras, .003, pSpec.NumberOfTriggers, .004, false);
+                Output("No Capture Specs defined");
+                return;
+            }
+
+            /// @todo - this should be made part of the SIM Device....
+            int numCameras = 0;
+            for (int i = 0; i < d.NumberOfCameras; i++)
+                if (d.GetSIMCamera(i).Status() == (CameraStatus)1)
+                    numCameras++;
+
+            for (int i = 0; i < d.NumberOfCaptureSpecs; i++)
+            {
+                ManagedMosaicLayer layer = _mosaicSet.AddLayer(.2, i * .2, numCameras, .003, pSpec.NumberOfTriggers, .004, false);
+
+                if (layer == null)
+                {
+                    Output("Could not create Layer: " + i);
+                    return;
+                }
+
+                /// Set up the transform parameters...
+                for (int j = 0; j < numCameras; j++)
+                {
+                    ManagedSIMCamera camera = d.GetSIMCamera(i);
+                    for (int k = 0; k < pSpec.NumberOfTriggers; k++)
+                    {
+                        ManagedMosaicTile mmt = layer.GetTile(j, k);
+
+                        if (mmt == null)
+                        {
+                            Output("Could not access tile at: " + j + ", " + k);
+                            return;
+                        }
+
+                        // Trigger offset is initial offset + triggerIndex * overlap...
+                        double triggerOffset = .2;
+                        mmt.SetTransformParameters(camera.Pixelsize.X, camera.Pixelsize.Y,
+                            triggerOffset, d.YOffset + camera.CenterOffset.Y, camera.Rotation);
+                    }
+                }
             }
         }
 
@@ -154,8 +187,9 @@ namespace CyberStitchTester
         private static void OnFrameDone(ManagedSIMFrame pframe)
         {
             Output("Got an Image!");
-
-            _mosaicSet.AddImage(pframe.BufferPtr(), pframe.CaptureSpecIndex(), pframe.CameraIndex(),
+            int layer = pframe.DeviceIndex()*ManagedCoreAPI.GetDevice(0).NumberOfCaptureSpecs +
+                        pframe.CaptureSpecIndex();
+            _mosaicSet.AddImage(pframe.BufferPtr(), layer, pframe.CameraIndex(),
                                 pframe.TriggerIndex());
         }
 
