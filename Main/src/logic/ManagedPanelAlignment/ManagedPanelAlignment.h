2361b331a9ab79bcafce89a8b4b171c6f483c64a
gzhou <gzhou@msp.tfs.local>
2010-11-17 17:56:22 +0000

    Add reference for ManagedPanelAlignment
    git-tfs-id: [https://devops.cyberoptics.com/TPC1]$/CyberStitch;C30022

--- Main/src/logic/ManagedPanelAlignment/ManagedPanelAlignment.h
+++ Main/src/logic/ManagedPanelAlignment/ManagedPanelAlignment.h
@@ -2,12 +2,29 @@
 
 #pragma once
 
+#include "PanelAligner.h"
+
 using namespace System;
+using namespace MMosaicDM; 
+using namespace Cyber::SPIAPI;
 
-namespace ManagedPanelAlignment {
+namespace PanelAlignM {
 
-	public ref class Class1
+	public ref class ManagedPanelAlignment
 	{
 		// TODO: Add your methods for this class here.
+
+		//bool SetPanel(ManagedMosaicSet^ set, CPanel^ panel);
+		//bool AddImage(
+		//	unsigned int iLayerIndex, 
+		//	unsigned int iTrigIndex, 
+		//	unsigned int iCamIndex,
+		//	unsigned char* pcBuf);
+
+		//LoggableObject* GetLogger() {return &LOG;};
+
+
+	private:
+		PanelAligner* _pAlinger;
 	};
 }
