44ce74f9c61fa873be22fd1a725a58aa7283c9d5
gzhou <gzhou@msp.tfs.local>
2010-11-17 13:53:25 +0000

    Add ManangedPanelAlignment class
    git-tfs-id: [https://devops.cyberoptics.com/TPC1]$/CyberStitch;C30015

---
+++ Main/src/logic/ManagedPanelAlignment/ManagedPanelAlignment.h
@@ -0,0 +1,13 @@
+// ManagedPanelAlignment.h
+
+#pragma once
+
+using namespace System;
+
+namespace ManagedPanelAlignment {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
