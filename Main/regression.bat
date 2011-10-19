17aaff5c7f785f5e7f50ec52545e7d39123dbc97
Alan Torborg <atorborg@cyberoptics.com>
2011-10-19 22:12:59 +0000

    Updates for regression test.
    git-tfs-id: [https://devops.cyberoptics.com/TPC1]$/CyberStitch;C38070

--- Main/regression.bat
+++ Main/regression.bat
@@ -12,7 +12,7 @@ REM		ALTA DATA
 set APP=.\src\Applications\CyberStitchFidTester\bin\x64\Release\CyberStitchFidTester.exe
 set SIMDATA=\\msp\dfs\archive\colorSimBoards\RegressionTest2\
 set CADDIR=\\msp\dfs\archive\colorSimBoards\PPM\
-%APP% -s "%SIMDATA%SIMScenario.xml" -p "%CADDIR%PPMTestPanel-HighRes2Fids(diagonal).xml" -f "%CADDIR%PPMTestPanel-HighResAllFidsNoPads.xml" -o .\RegressionResults\2Fids.txt -n 23
+%APP% -s "%SIMDATA%SIMScenario.xml" -p "%CADDIR%PPMTestPanel-HighRes2Fids(diagonal).xml" -f "%CADDIR%PPMTestPanel-HighResAllFidsNoPads.xml" -o .\RegressionResults\2Fids.txt -n 6
 
 REM		Test that the results are the same as last time
 REM TODO .\src\Applications\DBCompare\bin\Release\DBCompare.exe -1 c:\SentryData\Sentry.yap -2 c:\SentryDataBackup\Sentry.yap -o .\DBCompareResults.txt
