fea2ad17a64a4df559fcd2c61336c8f0c643a91a
Carl Haugan <chaugan@cyberoptics.com>
2012-05-14 22:24:20 +0000

    should have checked in along with the .cpp file
    git-tfs-id: [https://devops.cyberoptics.com/TPC1]$/CyberStitch;C43868

--- Main/src/logic/ImageCorrelation/CorrelationParameters.h
+++ Main/src/logic/ImageCorrelation/CorrelationParameters.h
@@ -73,6 +73,12 @@ public:
 	double dMaxFidDisScaleDiff;				// Max Fiducial distnace scale that can be tolerate
 	double dMaxSameFidInConsist;			// Max fiducial alignment result inconsist for the same fiducial (in meter)
 
+	// board scale test (for RobustSolverCM::FlattenFiducial() )
+	// these might be tied to the Fiducial check parameters above
+	double dAlignBoardStretchLimit;			// units meter / meter   maximum that the panel can shrink stretch
+	// IPC-D-300G which allows a stretch of 200 um over 300 mm or 0.067%
+	double dAlignBoardSkewLimit;			// units meter / meter maximum skew of board (in affine transform)
+
 	// Expansion from Cad image to create mask image in pixels
 	int iMaskExpansionFromCad;
 
