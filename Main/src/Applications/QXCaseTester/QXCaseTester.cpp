8e70bfe2c06c961ee99faf92879ec7c5f2240e76
gzhou <gzhou@msp.tfs.local>
2013-01-31 17:11:23 +0000

    A little reorganization
    git-tfs-id: [https://devops.cyberoptics.com/TPC1]$/CyberStitch;C52117

--- Main/src/Applications/QXCaseTester/QXCaseTester.cpp
+++ Main/src/Applications/QXCaseTester/QXCaseTester.cpp
@@ -27,29 +27,31 @@ bool RunStitch();
 
 clock_t _startTime;
 
+// Default paramter for SIM install to front 
 int _iBayerType = 1; // GBRG, 
+// default parameter for SIM 110
 int _iImCols = 2592;
 int _iImRows = 1944;
 double _dNominalPixelSize = 1.7e-5;
 
+// Required inputs
 string _sSimulationFile = "";	
 double _dPanelX = 0;
 double _dPanelY = 0;
 
+// Optional inputs
 int _iNumThreads = 8;
 int _iNumToRun = 1;
 
+// Interal variable
 Panel* _pPanel = NULL;
 MosaicSet* _pMosaicSet = NULL;
 PanelAligner* _pAligner = NULL;
-
 HANDLE _AlignDoneEvent;
 
 int main(int argc, char* argv[])
 {
-	remove("C:\\Temp\\QXCybetstitch.log");
-	_startTime = clock();
-
+	// Parameter input
 	for(int i=0; i<argc; i++)
 	{
 		string cmd = argv[i];
@@ -60,10 +62,13 @@ int main(int argc, char* argv[])
 			_dPanelX = atof(argv[i+1]);
 		else if(cmd == "-ys" && i <= argc-1)
 			_dPanelY = atof(argv[i+1]);
-		else if(cmd == "-ys" && i <= argc-1)
+		else if(cmd == "-t" && i <= argc-1)
 			_iNumThreads = atoi(argv[i+1]);
-	}
-
+		else if(cmd == "-n" && i <= argc-1)
+			_iNumToRun = atoi(argv[i+1]);
+	}	
+	
+	// Input validation check
 	if(_dPanelX==0 || _dPanelY==0)
 	{
 		Output("Panel Size is not available");
@@ -75,15 +80,24 @@ int main(int argc, char* argv[])
 		return(0);
 	}
 
+	// Prepare for log and event
+	remove("C:\\Temp\\QXCybetstitch.log");
+	_startTime = clock();
 	_AlignDoneEvent = CreateEvent(NULL, FALSE, FALSE, NULL); // Auto Reset, nonsignaled
 	
 	// Set up aligner
 	if(!SetupAligner())
+	{
 		Output("Failed to setup aligner");
+		return(0);
+	}
 	
 	// Do Stitch work
 	if(!RunStitch())
+	{
 		Output("Failed to run stitch");
+		return(0);
+	}
 
 	// Clean up
 	if(_pPanel != NULL)
@@ -92,14 +106,14 @@ int main(int argc, char* argv[])
 		delete _pMosaicSet;
 	if(_pAligner != NULL)
 		delete _pAligner;
-
 	CloseHandle(_AlignDoneEvent);
 
-	printf("Done!\n");
+	Output("Done!");
 	return 0;
 }
 
-
+#pragma region Set mosaicSet
+// leftM[8] * rightM[8] = outM[8]
 void MultiProjective2D(double* leftM, double* rightM, double* outM)
 {
     outM[0] = leftM[0]*rightM[0]+leftM[1]*rightM[3]+leftM[2]*rightM[6];
@@ -115,9 +129,7 @@ void MultiProjective2D(double* leftM, double* rightM, double* outM)
 	double dScale = leftM[6]*rightM[2]+leftM[7]*rightM[5]+1*1;
 
 	if(dScale<0.01 && dScale>-0.01)
-	{
 		dScale = 0.01;
-	}
 
 	for(int i=0; i<8; i++)
 		outM[i] = outM[i]/dScale;
@@ -134,6 +146,7 @@ void Pixel2World(double* trans, double row, double col, double* px, double* py)
     *py *= dScale;
 }
 
+// Calculate half size of FOV in world space
 void CalFOVHalfSize(double* trans, unsigned int iImW, unsigned int iImH, float* pfHalfW, float* pfHalfH)
 {
     // calcualte width
@@ -149,8 +162,9 @@ void CalFOVHalfSize(double* trans, unsigned int iImW, unsigned int iImH, float* 
     *pfHalfH = (float)((dBottomx-dTopx)/2.0);
 }
 
-bool SetMosaicSetConfig(string file, Panel* pPanel)
+bool SetMosaicSetConfig(MosaicSet* pSet, Panel* pPanel, string file)
 {
+//*** Read data from file 
 	char temp[1000];
 	ifstream in(file.c_str());
 
@@ -219,6 +233,7 @@ bool SetMosaicSetConfig(string file, Panel* pPanel)
 		iCount++;
 	}
 
+//*** Setup nominal transforms, s and dsdz
     // Add a mosaic layer
 	unsigned int iNumCam = (unsigned int)transList.size()/8;
 	unsigned int iNumTrig = (unsigned int)trigList.size() + 1;
@@ -228,7 +243,7 @@ bool SetMosaicSetConfig(string file, Panel* pPanel)
     bool bAlignWithFiducial = true;
     bool bFiducialBrighterThanBackground = true;
     unsigned int deviceIndex = 0;
-	MosaicLayer* pLayer = _pMosaicSet->AddLayer(iNumCam, iNumTrig, bAlignWithCAD, bAlignWithFiducial, bFiducialBrighterThanBackground, bFiducialAllowNegativeMatch, deviceIndex);
+	MosaicLayer* pLayer = pSet->AddLayer(iNumCam, iNumTrig, bAlignWithCAD, bAlignWithFiducial, bFiducialBrighterThanBackground, bFiducialAllowNegativeMatch, deviceIndex);
 
 	// Set subDevice
     if (iNumCam > 8)
@@ -236,13 +251,12 @@ bool SetMosaicSetConfig(string file, Panel* pPanel)
         list<unsigned int> iLastCams;
 		iLastCams.push_back(7); 
 		iLastCams.push_back(15);
-        _pMosaicSet->AddSubDeviceInfo(deviceIndex, iLastCams);
+        pSet->AddSubDeviceInfo(deviceIndex, iLastCams);
     }
 
-     // Set nominal transforms
-	//*** warning double check 
+	// QX (U, v)->(x,y) to Cyberstitch (U, v)->(x,y) conversion matrix
 	double dPanelHeight = pPanel->xLength();
-	unsigned int iImageRows = _pMosaicSet->GetImageHeightInPixels();
+	unsigned int iImageRows = pSet->GetImageHeightInPixels();
     double leftM[8] = { 
         0, -1, dPanelHeight-dOffsetY, 
         1, 0, dOffsetX,
@@ -255,9 +269,9 @@ bool SetMosaicSetConfig(string file, Panel* pPanel)
 
     double tempM[8];
     double camM[8];
-    double fovM[9];
+    double fovM[9];	// Must be 9
 
-    for (unsigned int iCam = 0; iCam < iNumCam; iCam++)
+    for (unsigned int iCam = 0; iCam < iNumCam; iCam++) // For each camera
     {
         // Calculate camera transform for first trigger
         MultiProjective2D(leftM, pTrans+iCam*8, tempM);
@@ -266,7 +280,7 @@ bool SetMosaicSetConfig(string file, Panel* pPanel)
         for (int i = 0; i < 8; i++)
             fovM[i] = camM[i];
 
-        for (unsigned int iTrig = 0; iTrig < iNumTrig; iTrig++)
+        for (unsigned int iTrig = 0; iTrig < iNumTrig; iTrig++) // For each trigger
         {
             // Set transform for each trigger
             if (iTrig > 0)
@@ -278,8 +292,8 @@ bool SetMosaicSetConfig(string file, Panel* pPanel)
             // For camera model 
             pTile->ResetTransformCamCalibration();
             pTile->ResetTransformCamModel();
-            pTile->SetTransformCamCalibrationUMax(_pMosaicSet->GetImageWidthInPixels());  // column
-            pTile->SetTransformCamCalibrationVMax(_pMosaicSet->GetImageHeightInPixels()); // row
+            pTile->SetTransformCamCalibrationUMax(pSet->GetImageWidthInPixels());  // column
+            pTile->SetTransformCamCalibrationVMax(pSet->GetImageHeightInPixels()); // row
 
             float Sy[16];
             float Sx[16];
@@ -301,9 +315,9 @@ bool SetMosaicSetConfig(string file, Panel* pPanel)
                 // dS
             double dPupilDistance = 0.3702;
             float fHalfW, fHalfH;
-            CalFOVHalfSize(camM, _pMosaicSet->GetImageWidthInPixels(), _pMosaicSet->GetImageHeightInPixels(), &fHalfW, &fHalfH);
-            dSydz[1] = (float)(fHalfW / dPupilDistance);   // dY/dZ
-            dSxdz[4] = (float)(fHalfH / dPupilDistance);  // dX/dZ
+            CalFOVHalfSize(camM, pSet->GetImageWidthInPixels(), pSet->GetImageHeightInPixels(), &fHalfW, &fHalfH);
+            dSydz[1] = (float)(fHalfW / dPupilDistance);	// dY/dZ
+            dSxdz[4] = (float)(fHalfH / dPupilDistance);	// dX/dZ
 
             pTile->SetTransformCamCalibrationS(0, Sy);
             pTile->SetTransformCamCalibrationS(1, Sx);
@@ -318,7 +332,8 @@ bool SetMosaicSetConfig(string file, Panel* pPanel)
 	delete [] pTrans;
 	delete [] pTrigs;
 
-	CorrelationFlags* pFlag = _pMosaicSet->GetCorrelationFlags(0, 0);
+	// Set correlation flag
+	CorrelationFlags* pFlag = pSet->GetCorrelationFlags(0, 0);
 	pFlag->SetCameraToCamera(true);
 	pFlag->SetTriggerToTrigger(true);
 
@@ -326,7 +341,7 @@ bool SetMosaicSetConfig(string file, Panel* pPanel)
 }
 
 
-bool SetUpMosaicSet(Panel* pPanel)
+bool SetUpMosaicSet(Panel* pPanel, string sSimulationFile)
 {
 	bool bSkipDemosaic = true;
 	bool bBayerPattern = true;
@@ -340,14 +355,17 @@ bool SetUpMosaicSet(Panel* pPanel)
         bOwnBuffers,
         bBayerPattern, _iBayerType, bSkipDemosaic);
 
-
-	bool bFlag = SetMosaicSetConfig(_sSimulationFile, pPanel);
+	// Setup mosaicset configuration
+	bool bFlag = SetMosaicSetConfig(_pMosaicSet, pPanel, sSimulationFile);
 
 	return(bFlag);
 }
 
+#pragma endregion
 
+#pragma region image load, log and call back
 Bitmap* _pBmps = NULL;  
+// load all raw image for a mosaic set from a file
 bool LoadAllRawImages(MosaicSet* pSet, string sFolder)
 {
     MosaicLayer* pLayer = pSet->GetLayer(0);
@@ -374,9 +392,11 @@ bool LoadAllRawImages(MosaicSet* pSet, string sFolder)
     return (true);
 }
 
+// Realse all image buffers
 void ReleaseBmpBufs()
 {
-	delete [] _pBmps;
+	if(_pBmps!=NULL)
+		delete [] _pBmps;
 }
 
 // Log callback
@@ -408,33 +428,45 @@ void OnAlignmentDone(bool status)
 	SetEvent(_AlignDoneEvent);
 }
 
+#pragma endregion
+
 // Setup aligner
 bool SetupAligner()
 {
+	// Set panel
 	_pPanel = new Panel(_dPanelX, _dPanelY, _dNominalPixelSize, _dNominalPixelSize);
 
 	// Set up mosaic
-	SetUpMosaicSet(_pPanel);
+	SetUpMosaicSet(_pPanel, _sSimulationFile);
 
+	// Setup aligner
 	_pAligner = new PanelAligner();
+
+		// Set up log
 	_pAligner->GetLogger()->SetAllLogTypes(true);
 	_pAligner->GetLogger()->RegisterLoggingCallback(LoggingCallback);
-    //_pAligner->LogTransformVectors(true);
-    //_pAligner->LogOverlaps(true);
-    //_pAligner->LogFiducialOverlaps(true);
-	//_pMosaicSet->SetSeperateProcessStages(_bSeperateProcessStages);
 
-    // Set up aligner delegate
+		// Set up callback
 	_pAligner->RegisterAlignmentDoneCallback(OnAlignmentDone, _pAligner);
 
-    // Set up production for aligner
+		// Setup threads	
     _pAligner->NumThreads(_iNumThreads);
     _pMosaicSet->SetThreadNumber(_iNumThreads);
 
+		// Setup camera model
 	_pAligner->UseCameraModelStitch(true);
     _pAligner->UseProjectiveTransform(true);  // projective transform is assumed for camera model stitching
+	
+		// Skip demosaic
 	_pAligner->SetSkipDemosaic(true);
-		   
+
+		// For Debug
+	//_pAligner->LogTransformVectors(true);
+    //_pAligner->LogOverlaps(true);
+    //_pAligner->LogFiducialOverlaps(true);
+	_pMosaicSet->SetSeperateProcessStages(true);
+
+		   // Set up production for aligner
 	if (!_pAligner->ChangeProduction(_pMosaicSet, _pPanel))
 		return(false);
         				
@@ -465,9 +497,11 @@ bool RunStitch()
 			Output("Invalid folder");
 			break;
 		}
-
 		LoadAllRawImages(_pMosaicSet, sFolder);
-        WaitForSingleObject(_AlignDoneEvent, INFINITE);       
+        
+		// Wait alignment to be done
+		WaitForSingleObject(_AlignDoneEvent, INFINITE);  
+		ReleaseBmpBufs();     
 
         // Verify that mosaic is filled in...
         if (!_pMosaicSet->HasAllImages())
@@ -477,8 +511,6 @@ bool RunStitch()
 		}
         else
         {
-			ReleaseBmpBufs();
-
 			Output("End stitch cycle");
             iCycleCount++;                   
 
@@ -487,7 +519,7 @@ bool RunStitch()
 			string sStitchedImFile;
 			sprintf_s(cTemp, 100, "c:\\temp\\Stitched%d.bmp", iCycleCount);
 			sStitchedImFile.assign(cTemp);
-			
+	
 			_pMosaicSet->GetLayer(0)->SaveStitchedImage(sStitchedImFile);
 			Output("End creating and saving stitched image");
 		}
@@ -499,6 +531,3 @@ bool RunStitch()
 
 	return(true);
 }
-
-
-
